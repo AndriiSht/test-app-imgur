afe0de35882b0fff4d4010d78cfd80ed76b09d5f
andriishtoiko <andrii.shtoiko@gmail.com>
2021-11-15 01:03:23 +0200

    added dummy data

--- src/hooks/useFetchImgurData.ts
+++ src/hooks/useFetchImgurData.ts
@@ -1,13 +1,15 @@
 import { ImgurData } from "./types";
 import { useQuery } from "react-query";
 import { apiURL, queryNames } from "const";
+import dummyData from "const/fallbackData.json";
 
 const fetchImgurData = () => {
   return fetch(`${apiURL.baseURL}${apiURL.getMemes}`, {
     referrer: "", // to make imgur api work - it is blocking localhost
   })
     .then((resp) => resp.json())
-    .then((data) => data?.data || []);
+    .then((data) => data?.data || dummyData.data)
+    .catch(() => dummyData.data);
 };
 
 export const useFetchImgurData = () => {
