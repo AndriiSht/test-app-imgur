6052a8a6cd1490d3567c2ef2321bf6bfdf069326
andriishtoiko <andrii.shtoiko@gmail.com>
2021-11-15 09:32:46 +0200

    fetch error handling

--- src/hooks/useFetchImgurData.ts
+++ src/hooks/useFetchImgurData.ts
@@ -7,8 +7,13 @@ const fetchImgurData = () => {
   return fetch(`${apiURL.baseURL}${apiURL.getMemes}`, {
     referrer: "", // to make imgur api work - it is blocking localhost
   })
-    .then((resp) => resp.json())
-    .then((data) => data?.data || dummyData.data)
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      }
+      return dummyData;
+    })
+    .then((data) => data.data)
     .catch(() => dummyData.data);
 };
 
